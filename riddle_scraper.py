6efd6fc099a104cdf189476ac8b4c0b9194c1ea9
remo5000 <vigneshshankar@gmail.com>
2017-12-31 19:14:55 +0800

    finish basic scraper using BeautifulSoup

--- riddle_scraper.py
+++ riddle_scraper.py
@@ -1,3 +1,10 @@
 from bs4 import BeautifulSoup 
 page_content = open("riddles_and_answers.html").read()
 soup = BeautifulSoup(page_content, 'html.parser')
+articles = soup.find_all('article')
+riddles = {}
+for article in articles:
+    question = article.find('h2').a.string
+    answer = article.find_all('div', class_='su-spoiler-content')[0].string
+    riddles[question] = answer
+print(riddles)
