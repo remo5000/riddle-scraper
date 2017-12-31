a3af8d17f673dc95d2ac336064a1476d8286390d
remo5000 <vigneshshankar@gmail.com>
2017-12-31 19:52:15 +0800

    mod readme, working selenium scraper using chromedriver

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ This is a simple scraper I made to harvest riddles from riddles.fyi.
 
 # Requirements
 Check any imports used in the python files and install them using pip3.
-A Chromedriver executable is to be placed at the root folder (not included here because its too big)
+A 'config.py' file of the format:
+	path_to_chromedriver = "/PATH/TO/CHROMEDRIVER"
+Chromedriver is an executable file used to control Chrome. check it out at https://sites.google.com/a/chromium.org/chromedriver/ and download it onto your computer to use for scraping.
--- selenium_riddle_scraper.py
+++ selenium_riddle_scraper.py
@@ -1,22 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
+from config import path_to_chromedriver
  
 def scrape_url(url):
  
     # Load WebDriver and navigate to the page url.
     # This will open a browser window.
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(path_to_chromedriver)
     driver.get(url)
      
     urls = []
  
     # First scroll to the end of the table by sending Page Down keypresses to
     # the browser window.
-    for x in range(1, 100):
+    for x in range(1, 10):
         # Find the first element on the page, so we can scroll down using the
         # element object's send_keys() method
         elem = driver.find_element_by_tag_name('a')
         elem.send_keys(Keys.PAGE_DOWN)
+        sleep(3)
      
     # Once the whole table has loaded, grab all the visible links.    
     articles  = driver.find_elements_by_tag_name('article')
