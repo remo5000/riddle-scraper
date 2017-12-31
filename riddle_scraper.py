3c1693e04395905646d9b712aa9cf6a23a5010ab
remo5000 <vigneshshankar@gmail.com>
2017-12-31 22:58:01 +0800

    print a list of JSON objects as the riddles

--- riddle_scraper.py
+++ riddle_scraper.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup 
 from selenium_infinite_html_scraper import scrape_url_to_html
+import json
 
-riddle_articles = scrape_url_to_html("https://riddles.fyi/","article", 14)
+riddle_articles = scrape_url_to_html("https://riddles.fyi/","article", 70)
 # soup = BeautifulSoup(page_content, 'html.parser')
 # articles = soup.find_all('article')
-riddles = {}
+riddles = [] 
+index = 9
+# use BeautifulSoup to get the question and answer from each article elem
 for article in riddle_articles:
     article = BeautifulSoup(article, 'html.parser')
-    question = article.find('h2').a.string
-    answer = article.find_all('div', class_='su-spoiler-content')[0].string
-    riddles[question] = answer
-print(riddles)
+    riddle_obj = {}
+    riddle_obj["ID"] = str(index)
+    riddle_obj['Question'] =  article.find('h2').a.string
+    riddle_obj['Answer'] = article.find_all('div', class_='su-spoiler-content')[0].string
+    riddles.append(riddle_obj)
+    index += 1
+print(json.dumps(riddles))
 print("number of riddles found:", len(riddles))
