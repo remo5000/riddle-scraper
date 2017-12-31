74308370ecdaa89da8d57c4ef37137833a6e4761
remo5000 <vigneshshankar@gmail.com>
2017-12-31 22:12:29 +0800

    use selenium to accumulate articles, and beautifulsoup to only acquire question and answer

--- riddle_scraper.py
+++ riddle_scraper.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup 
 from selenium_infinite_html_scraper import scrape_url_to_html
 
-page_content = scrape_url_to_html("https://riddles.fyi/", 20)
-soup = BeautifulSoup(page_content, 'html.parser')
-articles = soup.find_all('article')
+riddle_articles = scrape_url_to_html("https://riddles.fyi/","article", 14)
+# soup = BeautifulSoup(page_content, 'html.parser')
+# articles = soup.find_all('article')
 riddles = {}
-for article in articles:
+for article in riddle_articles:
+    article = BeautifulSoup(article, 'html.parser')
     question = article.find('h2').a.string
     answer = article.find_all('div', class_='su-spoiler-content')[0].string
     riddles[question] = answer
 print(riddles)
+print("number of riddles found:", len(riddles))
--- selenium_infinite_html_scraper.py
+++ selenium_infinite_html_scraper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 from config import path_to_chromedriver
  
-def scrape_url_to_html(url, number_of_scrolls = 5):
+def scrape_url_to_html(url, tag_name = "html", number_of_scrolls = 5):
  
     # Load WebDriver and navigate to the page url.
     # This will open a browser window.
@@ -12,15 +12,22 @@ def scrape_url_to_html(url, number_of_scrolls = 5):
      
     # First scroll to the end of the table by sending Page Down keypresses to
     # the browser window.
+    result_set = set()
     for x in range(number_of_scrolls):
+        # Add whatever content we can find using the element tag provided
+        current_results = driver.find_elements_by_tag_name(tag_name)
+        for res in current_results:
+            result_set.add(res.get_attribute('innerHTML'))
         # Find the first element on the page, so we can scroll down using the
         # element object's send_keys() method
         elem = driver.find_element_by_tag_name('a')
+        # Down, up, and down movement to faciliate loading
+        elem.send_keys(Keys.END)
+        elem.send_keys(Keys.HOME)
         elem.send_keys(Keys.END)
         # Wait 3 seconds for the infinitely loading page to load.
         sleep(3)
+        elem.send_keys(Keys.END)
      
-    # Once the whole table has loaded, grab all the riddles 
-    html_string = driver.page_source
     driver.quit()
-    return html_string
+    return list(result_set) 
