8cd5d1d5d712ad0256e6ff837c155a16c89782b4
remo5000 <vigneshshankar@gmail.com>
2017-12-31 20:21:00 +0800

    changed selenium scraper to only return html

--- selenium_riddle_scraper.py
+++ selenium_riddle_scraper.py
@@ -3,31 +3,27 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 from config import path_to_chromedriver
  
-def scrape_url(url):
+def scrape_url_to_html(url, number_of_scrolls = 30):
  
     # Load WebDriver and navigate to the page url.
     # This will open a browser window.
     driver = webdriver.Chrome(path_to_chromedriver)
     driver.get(url)
      
-    urls = []
- 
     # First scroll to the end of the table by sending Page Down keypresses to
     # the browser window.
-    for x in range(1, 10):
+    for x in range(number_of_scrolls):
         # Find the first element on the page, so we can scroll down using the
         # element object's send_keys() method
         elem = driver.find_element_by_tag_name('a')
         elem.send_keys(Keys.PAGE_DOWN)
+        # Wait 3 seconds for the infinitely loading page to load.
         sleep(3)
      
-    # Once the whole table has loaded, grab all the visible links.    
-    articles  = driver.find_elements_by_tag_name('article')
-    print(len(articles))
-    #for link in visible_links:
-    #    urls.append(link.get_attribute('href'))
-         
+    # Once the whole table has loaded, grab all the riddles 
+    html_string = driver.page_source
     driver.quit()
-           
-    return urls
-scrape_url("https://riddles.fyi/")
+    return html_string
+
+html_string = scrape_url_to_html("https://riddles.fyi/", 5)
+print(html_string)
